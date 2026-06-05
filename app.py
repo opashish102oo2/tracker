bae7eef34c44b34b3ff399e029ea9debc16f7963
Ashish <akashish1200a@gmail.com>
2026-06-05 23:00:24 +0530

    fixed api

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Cloudinary Configuration
 cloudinary.config(
     cloud_name="dqsetcecu",
-    api_key="CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dqsetcecu",
+    api_key="119182683135652",
     api_secret="9Qbv-lCwg7oN_dkDtjHESAbhLIw"
 )
 
